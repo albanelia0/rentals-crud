2c1417f18b7921c42f7cf068818e080bcb5c2dd0
Alba <albanelia0@gmail.com>
2021-05-21 19:37:28 +0200

    Add react-router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import './App.css'
 import ListPage from './pages/ListPage'
 
 function App (): JSX.Element {
   return (
-    <div>
-      <ListPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' component={ListPage} />
+      </Switch>
+    </Router>
   )
 }
 
