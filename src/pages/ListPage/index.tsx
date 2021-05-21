01512994d095bf59b76ca0232955a75bcfee2e06
Alba <albanelia0@gmail.com>
2021-05-21 17:23:24 +0200

    Create list page

---
+++ src/pages/ListPage/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import RentalsList from '../../components/RentalsList'
+
+interface ApiListItem {
+  id: number
+  title: string
+  image: string
+  price: number
+  desc: string
+}
+
+type ApiList = ApiListItem[]
+
+const ListPage = (): JSX.Element => {
+  const [rentals, setRentals] = useState<ApiList>([])
+
+  useEffect(() => {
+    window.fetch('http://localhost:3030/real-estate')
+      .then(async res => await res.json())
+      .then(apiItems => setRentals(prev => [...prev, ...apiItems]))
+      .catch(console.error)
+  }, [])
+
+  console.log('rentals', rentals)
+  return (
+    <div>
+      <RentalsList rentals={rentals} />
+    </div>
+  )
+}
+
+export default ListPage
