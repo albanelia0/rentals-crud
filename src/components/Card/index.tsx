9c90553ec3f05dba67a3b569ec1b5c7f54dc2693
Alba <albanelia0@gmail.com>
2021-05-22 18:14:05 +0200

    Add pencil to card and event click to edit page

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 import styles from './index.module.css'
-
+import { ReactComponent as Pencil } from './icons/pencil.svg'
+import { useHistory } from 'react-router-dom'
 interface CardProps {
   id: number
   title: string
@@ -9,9 +10,17 @@ interface CardProps {
 }
 
 const Card = ({ id, image, title, price, desc }: CardProps): JSX.Element => {
+  const history = useHistory()
+
+  const handleEdit = (): void => {
+    history.push(`/edit/${id}`)
+  }
   return (
     <div className={styles.wrapper}>
-      <img alt='article' src={image} />
+      <div className={styles.contentImg}>
+        <img alt='article' src={image} />
+        <span onClick={handleEdit} className={styles.pencil}><Pencil /></span>
+      </div>
       <div className={styles.title}>{title}</div>
       <div className={styles.desc}>{desc}</div>
       <div><strong>{price} €</strong> / night</div>
