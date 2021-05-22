3af44208409bbf7132eaeabf208a182ffe2cf92e
Alba <albanelia0@gmail.com>
2021-05-22 18:12:59 +0200

    Create RentalForm component

--- src/components/RentalForm/index.tsx
+++ src/components/RentalForm/index.tsx
@@ -1,21 +1,64 @@
+import { useState, ChangeEventHandler, FormEventHandler } from 'react'
 import styles from './index.module.css'
 
-const RentalForm = (): JSX.Element => {
+export interface FormData {
+  title: string
+  desc: string
+  image: string
+  price: string
+}
+
+interface FormProps {
+  onSubmit: (data: FormData) => void
+  initialData?: FormData
+}
+
+const INITIAL_DATA = {
+  title: '',
+  desc: '',
+  image: '',
+  price: ''
+}
+
+const RentalForm = ({
+  onSubmit,
+  initialData = INITIAL_DATA
+}: FormProps): JSX.Element => {
+  const [data, setData] = useState<FormData>(initialData)
+
+  const handleSubmit: FormEventHandler = (event): void => {
+    event.preventDefault()
+    onSubmit(data)
+  }
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const fieldName = event.currentTarget.name
+    const newValue = event.currentTarget.value
+    setData(prev => ({
+      ...prev,
+      [fieldName]: newValue
+    }))
+  }
+
   return (
-    <form className={styles.wrapper}>
+    <form className={styles.wrapper} onSubmit={handleSubmit}>
       <div className={styles.content}>
         <label>Title</label>
-        <input type='text' />
+        <input name='title' value={data.title} onChange={handleChange} type='text' />
+      </div>
+      <div className={styles.content}>
+        <label>description</label>
+        <input name='desc' value={data.desc} onChange={handleChange} type='text' />
       </div>
       <div className={styles.content}>
         <label>Url image</label>
-        <input type='text' />
+        <input name='image' value={data.image} onChange={handleChange} type='text' />
       </div>
       <div className={styles.content}>
         <label>price</label>
-        <input type='text' />
+        <input name='price' value={data.price} onChange={handleChange} type='text' />
       </div>
-      <button> Publicate </button>
+      <button type='submit'>Publish</button>
     </form>
   )
 }
--- src/pages/CreatePage/index.tsx
+++ src/pages/CreatePage/index.tsx
@@ -1,11 +1,25 @@
-import RentalForm from '../../components/RentalForm'
+import { useHistory } from 'react-router-dom'
+
+import RentalForm, { FormData } from '../../components/RentalForm'
 import styles from './index.module.css'
 
 const CreatePage = (): JSX.Element => {
+  const history = useHistory()
+
+  const handleSubmit = (data: FormData): void => {
+    window
+      .fetch('http://localhost:3030/real-estate', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      })
+      .then((): void => history.push('/'))
+      .catch(console.error)
+  }
   return (
     <div>
       <h3 className={styles.title}>Create rental</h3>
-      <RentalForm />
+      <RentalForm onSubmit={handleSubmit} />
     </div>
   )
 }
